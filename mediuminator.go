ea7f5ca682db0a97e105333f2d85538263b22dec
Ed Summers <ehs@pobox.com>
2014-02-25 22:18:32 -0500

    only include tweets that have medium.com url in them ; print out details of tweet

--- mediuminator.go
+++ mediuminator.go
@@ -2,22 +2,11 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/darkhelmet/twitterstream"
 	"log"
+	"strings"
 )
-
-func listenForTweets(consumerKey string, consumerSecret string, accessToken string, accessSecret string) {
-	twitter := twitterstream.NewClient(consumerKey, consumerSecret, accessToken, accessSecret)
-	conn, err := twitter.Track("medium com")
-	if err != nil {
-		log.Fatal("unable to connect to twitter", err)
-	}
-	for {
-		if tweet, err := conn.Next(); err == nil {
-			log.Println(tweet.Text)
-		}
-	}
-}
 
 func main() {
 	consumerKey := flag.String("consumer-key", "", "consumer key")
@@ -27,4 +16,23 @@ func main() {
 	flag.Parse()
 
 	listenForTweets(*consumerKey, *consumerSecret, *accessToken, *accessSecret)
+}
+
+func listenForTweets(consumerKey string, consumerSecret string, accessToken string, accessSecret string) {
+	twitter := twitterstream.NewClient(consumerKey, consumerSecret, accessToken, accessSecret)
+	conn, err := twitter.Track("medium com")
+	if err != nil {
+		log.Fatal("unable to connect to twitter", err)
+	}
+	for {
+		if tweet, err := conn.Next(); err == nil {
+			for _, url := range tweet.Entities.Urls {
+				if !strings.Contains(*url.ExpandedUrl, "medium.com") {
+					continue
+				}
+				tweetUrl := "http://twitter.com/" + tweet.User.ScreenName + "/status/" + tweet.IdString
+				fmt.Println(tweetUrl, tweet.Text, *url.ExpandedUrl)
+			}
+		}
+	}
 }
